feb2cc11bc7e44fdc30b751f9749189a3c75d61d
Tammy-le <tamaraleontidis@web.de>
2019-09-04 18:58:08 +0200

    Introduced Vectors

--- src/Game.ts
+++ src/Game.ts
@@ -2,25 +2,92 @@
 
 export function setup() {
     createCanvas(windowWidth, windowHeight);
+    // x = windowWidth/2
+    // y = windowHeight/2
+}
+const DIAMETER=10
+// let x;
+// let x_delta=2;
+// let y;
+// let y_delta=0;
+
+
+let position = [20,0];
+let movement = [0,20];
+function getX(arr){
+    return arr [0]
+}
+function getY(arr){
+    return arr [1]
 }
 
-let x=0;
-let x_delta=2;
-let y=0;
-let y_delta=0;
+function isTopOutOfBounds(arr){
+    let y = getY(arr)
+
+    if (y<=DIAMETER){
+        return true;
+    }
+
+    return false
+}
+
+function isBottomOutOfBounds(arr) {
+    let y = getY(arr);
+
+    if (y >= (windowHeight-DIAMETER)) {
+        return true;
+    }
+
+    return false;
+}
 
 export function draw() {
-    background(51);
+    background(256);
 
-    if (x<=(windowWidth-10)){
-        x=x+x_delta;
+    let topOutOfBounds = isTopOutOfBounds(position);
+    if (topOutOfBounds) {
+        let y = getY(movement);
+        if (y <= 0) {
+            movement[1] = movement[1] * -1;
+        }
     }
 
-    if (y<=(windowHeight-10)){
-        y=y+y_delta;
+    let bottomOutOfBounds = isBottomOutOfBounds(position);
+    if (bottomOutOfBounds) {
+        let y = getY(movement);
+        if (y >= 0) {
+            movement[1] = movement[1] * -1;
+        }
     }
 
-    circle(x,y,10);
+    // if (x<=(windowWidth-DIAMETER)){
+    //     x=x+x_delta;
+    // }
+
+    // if (y<=(windowHeight-DIAMETER)){
+    //     y=y+y_delta;
+    // }
+
+    // if (x<=DIAMETER){
+    //     x=x-x_delta;
+    // }
+
+    // if (y<=DIAMETER){
+    //     y=y-y_delta;
+    // }
+
+
+    // Positions Update
+    let [x, y] = position;
+    let [xm, ym] = movement;
+    x += xm
+    y += ym
+    position = [x, y]
+ 
+
+    noStroke();
+    fill(206,0,0);
+    circle(x,y,DIAMETER);
 
 }
 
@@ -29,16 +96,16 @@ export function keyPressed() {
 
     switch (keyCode) {
         case LEFT_ARROW:
-            x_delta=-2;
+            movement = [-1, 0];
             break;
         case RIGHT_ARROW:
-            x_delta=2;
+            movement = [1, 0];
             break;
         case UP_ARROW:
-            y_delta=-2;
+            movement = [0, -1];
             break;
         case DOWN_ARROW:
-            y_delta=2;
+            movement = [0, 1];
             break;
     }
 }
